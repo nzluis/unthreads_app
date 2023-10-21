8460fed23fef42e040e2b002fa31b41588902103
luis <luisnavarroFS@gmail.com>
2023-10-21 19:06:09 +0200

    home page and layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,9 @@
-import { UserButton } from "@clerk/nextjs";
 
 
 export default function Home() {
   return (
     <main>
-      <UserButton afterSignOutUrl="/"/>
+      <h1 className="head-text text-left">Home</h1>
     </main>
   )
 }
