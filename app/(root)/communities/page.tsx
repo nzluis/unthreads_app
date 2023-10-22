ef7161b8381f971b21946bb2c9fdd7d971330822
luis <luisnavarroFS@gmail.com>
2023-10-22 20:28:22 +0200

    add init communities page

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,3 +1,13 @@
+const Page = async () => {
+    return (
+        <section>
+            <h1 className="head-text mb-10">
+                Communities
+            </h1>
+        </section>
+    )
+}
+
 // import { currentUser } from "@clerk/nextjs";
 // import { redirect } from "next/navigation";
 
